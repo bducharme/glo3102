cd0d23a8a340e3c2657d6830945e60d402e81664
Benoit Ducharme <ducharme_benoit@hotmail.com>
2015-09-17 16:53:27 -0400

    routing + fixed inject js/css path

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,16 @@ var inject = require('gulp-inject');
 
 gulp.task('default', function () {
     var target = gulp.src('./public/index.html');
-    var sources = gulp.src(['./public/bower_components/**/*.min.css', './public/css/*.css'], {read: false});
+    var sources = gulp.src(['./public/bower_components/**/*.min.css', './public/css/*.css',
+                            './public/bower_components/angular*/**/*.min.js', './public/app/**/*.js'],
+                            {read: false});
 
-    return target.pipe(inject(sources))
+    var injectOptions = {
+        ignorePath: 'public',
+        addRootSlash: false
+    };
+
+    return target.pipe(inject(sources, injectOptions))
         .pipe(gulp.dest('./public'));
 });
+
