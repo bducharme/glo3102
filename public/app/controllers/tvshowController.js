f4bd21fd9e15a2ddd78998688c233fc6826c5ce7
Philippe Dupuis <dupuis_philippe@hotmail.ca>
2015-11-03 11:47:58 -0500

    Suppresion de lignes non nécessaires

--- public/app/controllers/tvshowController.js
+++ public/app/controllers/tvshowController.js
@@ -19,20 +19,4 @@ angular.module('cornpub')
         },function(episode) {
            $scope.TVshowEpisodes = episode.results;
         });
-  })
-
-
-  .controller('AccordionCtrl', function ($scope) {
-        'use strict';
-
-        $scope.seasons = [
-            {
-                number: 'Season - 1',
-                episodes: [{'title': 'episode 1'}, {'title': 'episode 2'}, {'title': 'episode 3'}]
-            },
-            {
-                number: 'Season - 2',
-                episodes: [{'title': 'episode 1'}, {'title': 'episode 2'}, {'title': 'episode 3'}]
-            }
-        ];
-    });
+  });
