cd9eb20b80e3c5b55f14633a199d8e68d480941c
bducharme <ducharme_benoit@hotmail.com>
2015-11-02 22:23:25 -0500

    average autocomplete

--- public/app/controllers/navbarController.js
+++ public/app/controllers/navbarController.js
@@ -1,7 +1,21 @@
 angular.module('cornpub')
-    .controller('NavbarCtrl', function ($scope, SearchStringService) {
+    .controller('NavbarCtrl', function ($scope, $http, SearchStringService, baseURL) {
         'use strict';
 
         $scope.SearchStringService = SearchStringService;
+
+        $scope.getMovies = function() {
+          return $http.get(baseURL + '/unsecure/search', {
+            params: {
+              q: SearchStringService.searchString
+            }
+          }).then(function(response){
+            return response.data.results.map(function (movie) {
+              console.log(movie.trackName || movie.artistName);
+              return movie.trackName || movie.artistName;
+
+            });
+          });
+        };
 
     });
