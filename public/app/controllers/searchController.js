3250e7a5eeebf491bdb87e54ddc971fcd799f3ba
JDownloader <jeandanielpearson@gmail.com>
2015-11-02 19:05:04 -0500

    search is instantanious

--- public/app/controllers/searchController.js
+++ public/app/controllers/searchController.js
@@ -2,13 +2,17 @@ angular.module('cornpub')
     .controller('SearchCtrl', function ($scope, SearchService, SearchStringService) {
         'use strict';
 
-        $scope.$on('search', function() {
+        var doSearch = function(){
             SearchService.query({
                 q: SearchStringService.searchString
             }, function(response) {
                 $scope.searchResults = [];
                 $scope.searchResults.push(response);
             });
-        });
+        };
 
+        $scope.$on('search', doSearch);
+
+        doSearch();
+        console.log('here');
     });
