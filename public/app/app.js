41ab7667bac1447d3dd227994e6718eaa75f30ac
Samuel Gagnon <Samuel@Samuels-MacBook-Pro.local>
2015-11-04 22:57:48 -0500

    Actor page dynamised

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,9 @@
 angular.module('cornpub', ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap'])
-  .constant('baseURL', "http://localhost:3000")
-  //.constant('baseURL', "https://umovie.herokuapp.com")
+  //.constant('baseURL', "http://localhost:3000")
+  .constant('baseURL', "https://umovie.herokuapp.com")
 
   .config(function($logProvider) {
     'use strict';
 
     $logProvider.debugEnabled(true);
   });
-
-
-
