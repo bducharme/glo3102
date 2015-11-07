71d52bcb9c0b41e956c33c5fcf45e1bc5c792a34
Philippe Dupuis <dupuis_philippe@hotmail.ca>
2015-11-06 23:08:33 -0500

    Classer les saisons en ordre

--- public/app/controllers/tvshowController.js
+++ public/app/controllers/tvshowController.js
@@ -6,7 +6,13 @@ angular.module('cornpub')
             id: $stateParams.seasonId
         }, function(season) {
             var seasonResult = [];
+            var showName = season.results[0].artistName + ", Season ";
             for(var seasons = 1; seasons < season.results.length; seasons++) {
+                if(showName.length == season.results[seasons].collectionName.length + 1)
+                    season.results[seasons].numSeason = parseInt(season.results[seasons].collectionName.substring(showName.length, showName.length + 1));
+                else
+                    season.results[seasons].numSeason = parseInt(season.results[seasons].collectionName.substring(showName.length, showName.length + 2));
+
                 seasonResult.push(season.results[seasons]);
             }
             $scope.TVshowSeason = seasonResult;
