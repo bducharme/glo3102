959ec75cea5215075c6ccf69198b65a71d095537
adambbolduc <adam.b.bolduc@gmail.com>
2015-12-09 12:54:59 -0500

    Ajouter le service de preview

--- public/app/controllers/movieController.js
+++ public/app/controllers/movieController.js
@@ -1,32 +1,33 @@
 angular.module('cornpub')
-  .factory('movieFactory', function ($resource, baseURL) {
-    'use strict';
-    return $resource(baseURL +'/unsecure/movies/:id', {}, {
-      get: {method: 'GET', params: {id: '@id'}}
+    .controller('movieController', function ($scope, $stateParams, movieFactory, previewService) {
+        movieFactory.get({
+                id: $stateParams.movieId
+            }, function (data) {
+                $scope.movie = data.results[0];
+                $scope.movie.artworkUrl500 = $scope.movie.artworkUrl100.substring(0, $scope.movie.artworkUrl100.length - 13) + "500x500bb.jpg";
+                $scope.getVideoLink($scope.movie.trackName);
+            }
+        );
+
+        $scope.closeAlert = function () {
+            $scope.alert = false;
+        };
+
+        $scope.$on("movieAdded", function () {
+            $scope.alert = true;
+        });
+
+        $scope.$on('user', function (event, user) {
+            $scope.user = user;
+        });
+
+        $scope.getVideoLink = function(videoName) {
+            previewService.get({
+                mediaName: videoName+'official trailer'
+            }, function(preview){
+                $scope.videoLink = "http://www.youtube.com/embed/" + preview.items[0].id.videoId;
+            });
+        }
     });
-  });
 
-angular.module('cornpub')
-  .controller('movieController', function ($scope, $stateParams, $uibModal, movieFactory) {
 
-    movieFactory.get({
-        id: $stateParams.movieId
-      }, function(data) {
-        $scope.movie = data.results[0];
-        $scope.movie.artworkUrl500 = $scope.movie.artworkUrl100.substring(0, $scope.movie.artworkUrl100.length - 13)+"500x500bb.jpg";
-      }
-    );
-
-    $scope.closeAlert = function() {
-      $scope.alert = false;
-    };
-
-    $scope.$on("movieAdded", function() {
-      $scope.alert = true;
-    });
-
-    $scope.$on('user', function(event, user) {
-      $scope.user = user;
-    });
-
-  });
