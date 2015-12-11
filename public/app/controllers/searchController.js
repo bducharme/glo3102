9e166b450dee1ee53a64c0c11c1b3b1fbb9eb5c2
JDownloader <jeandanielpearson@gmail.com>
2015-12-10 21:36:40 -0500

    template and stuff

--- public/app/controllers/searchController.js
+++ public/app/controllers/searchController.js
@@ -1,5 +1,5 @@
 angular.module('cornpub')
-    .controller('SearchCtrl', function ($scope, SearchService, SearchServiceUser, SearchStringService) {
+    .controller('SearchCtrl', function ($scope, SearchService, SearchServiceUser, SearchStringService, md5) {
         'use strict';
 
         $scope.currentPage = 1;
@@ -21,11 +21,14 @@ angular.module('cornpub')
                 $scope.searchResults.push(response);
             });
         };
+
         var populateUsers = function(){
             SearchServiceUser.query({
                 q: SearchStringService.searchString
             }, function(response) {
                 $scope.searchUsersResults = [];
+
+                //$scope.user.md5 = md5.createHash(response.email);
                 $scope.searchUsersResults.push(response);
             });
         };
@@ -33,4 +36,26 @@ angular.module('cornpub')
         $scope.$on('search', doSearch);
 
         doSearch();
+
+        $scope.getGravatar = function(email){
+            return 'http://www.gravatar.com/avatar/'+md5.createHash(email);
+        }
+    }).directive('resultdetailsmovie', function() {
+        return {
+            templateUrl: function(){
+                return 'template/result-details-movie.html';
+            }
+        };
+    }).directive('resultdetailsartist', function() {
+        return {
+            templateUrl: function(){
+                return 'template/result-details-actor.html';
+            }
+        };
+    }).directive('resultdetailstvshow', function() {
+        return {
+            templateUrl: function(){
+                return 'template/result-details-tvshow.html';
+            }
+        };
     });
