e958741cebbbab9b1093e377cdab95d1d8384add
bducharme <ducharme_benoit@hotmail.com>
2015-12-13 21:40:12 -0500

    add/remove friend from search

--- public/app/controllers/searchController.js
+++ public/app/controllers/searchController.js
@@ -1,17 +1,9 @@
 angular.module('cornpub')
-    .controller('SearchCtrl', function ($scope, FriendFactory, SearchService, SearchServiceUser, SearchStringService, SearchServiceActor, md5) {
+    .controller('SearchCtrl', function($scope, FriendFactory, SearchService, SearchServiceUser, SearchStringService, SearchServiceActor, md5) {
         'use strict';
 
         $scope.currentPage = 1;
         $scope.pageSize = 10;
-
-        $scope.follow = function(id) {
-          FriendFactory.save({
-            id: id
-          }, function () {
-           // $scope.following.unshift($scope.user);
-          });
-        };
 
         var doSearch = function(){
             $scope.currentPage = 1;
@@ -63,8 +55,54 @@ angular.module('cornpub')
 
         $scope.getGravatar = function(email){
             return 'http://www.gravatar.com/avatar/'+md5.createHash(email);
-        }
-    }).directive('resultdetailsmovie', function() {
+        };
+
+
+        $scope.following = [];
+
+        $scope.$on('user', function (event, user) {
+          $scope.user = user;
+          $scope.me = user;
+          $scope.following = $scope.me.following;
+        });
+
+        $scope.addFriend = function(user) {
+          FriendFactory.save({
+            id: user.id
+          }, function () {
+            $scope.following.unshift(user);
+          });
+        };
+
+        $scope.removeFriend = function (friend) {
+          FriendFactory.remove({
+            id: friend.id
+          }, function () {
+            var toRemove = $scope.following.filter(function(user) {
+              return user.id == $scope.user.id;
+            })[0];
+            var index = $scope.following.indexOf(toRemove);
+            $scope.following.splice(index, 1);
+          });
+        };
+
+        $scope.isFriend = function(friends, friend) {
+          var isFriend = false;
+          if (friends !== undefined) {
+            friends.forEach(function (f) {
+              if(f.id === friend.id) {
+                isFriend = true;
+              }
+            })
+          }
+          return isFriend;
+        };
+
+
+
+
+
+  }).directive('resultdetailsmovie', function() {
         return {
             templateUrl: function(){
                 return 'template/result-details-movie.html';
